3d7f9da501f34af094c3b021f246d928d6cde049
Wes Smith <wes@w5mith.com>
2014-12-12 10:55:38 -0500

    add category for parsing/encoding/decoding query params

---
+++ Tests/Categories/NSString_BTNQuerySpec.m
@@ -0,0 +1,80 @@
+#import "Specta.h"
+#import "NSString+BTNQuery.h"
+
+SpecBegin(NSString_BTNQuery)
+
+NSString *plainTestString   = @"plain";
+NSString *decodedTestString = @"!*'();:@&=+$,/?%#[] ";
+NSString *encodedTestString = @"%21%2A%27%28%29%3B%3A%40%26%3D%2B%24%2C%2F%3F%25%23%5B%5D%20";
+
+
+describe(@"Percent Encoding", ^{
+    
+    it(@"encodes an extended set of illegal url characters", ^{
+        NSString *encodedString = [decodedTestString BTN_stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        expect(encodedString).to.equal(encodedTestString);
+    });
+    
+    it(@"does not encode a string without illegal characters", ^{
+        NSString *encodedString = [plainTestString BTN_stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        expect(encodedString).to.equal(plainTestString);
+    });
+});
+
+
+describe(@"Percent Decoding", ^{
+    
+    it(@"decodes a string with escaped url characters", ^{
+        NSString *decodedString = [encodedTestString BTN_stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        expect(decodedString).to.equal(decodedTestString);
+    });
+    
+    it(@"does not decode a string without escaped characters", ^{
+        NSString *decodedString = [plainTestString BTN_stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        expect(decodedString).to.equal(plainTestString);
+    });
+});
+
+
+describe(@"Dictionary to Query String", ^{
+    
+    it(@"should construct a query string from a dictionary", ^{
+        NSDictionary *params = @{ @"one": @1, @"two": @2 };
+        NSString *query = [NSString BTN_queryStringWithParameters:params];
+        expect(query).to.equal(@"one=1&two=2");
+    });
+    
+    it(@"should percent encode parameters from dictionary into the query string", ^{
+        NSDictionary *params = @{ @"one": @"a one", @"two": @"http://www.example.com" };
+        NSString *query = [NSString BTN_queryStringWithParameters:params];
+        expect(query).to.equal(@"one=a%20one&two=http%3A%2F%2Fwww.example.com");
+    });
+});
+
+
+describe(@"Query String to Dictionary", ^{
+   
+    it(@"should parse a valid query string into a dictionary", ^{
+        NSString *query = @"one=1&two=2";
+        NSDictionary *params = [query BTN_parametersFromQueryString];
+        expect(params[@"one"]).to.equal(@"1");
+        expect(params[@"two"]).to.equal(@"2");
+    });
+    
+    it(@"should ignore incomplete pairs in a query string", ^{
+        NSString *query = @"one=1&two&three=3";
+        NSDictionary *params = [query BTN_parametersFromQueryString];
+        expect(params[@"one"]).to.equal(@"1");
+        expect(params[@"two"]).to.beNil();
+        expect(params[@"three"]).to.equal(@"3");
+    });
+    
+    it(@"should decode query parameters into a dictionary", ^{
+        NSString *query = @"one=a%20one&two=http%3A%2F%2Fwww.example.com";
+        NSDictionary *params = [query BTN_parametersFromQueryString];
+        expect(params[@"one"]).to.equal(@"a one");
+        expect(params[@"two"]).to.equal(@"http://www.example.com");
+    });
+});
+
+SpecEnd
