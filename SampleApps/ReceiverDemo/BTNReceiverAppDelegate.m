72a71fa5bdee55d18e25798776e19ba7d61748ef
Wes Smith <wes@w5mith.com>
2014-12-18 00:50:39 -0500

    register a handler in the receiver demo

--- SampleApps/ReceiverDemo/BTNReceiverAppDelegate.m
+++ SampleApps/ReceiverDemo/BTNReceiverAppDelegate.m
@@ -1,8 +1,36 @@
 #import "BTNReceiverAppDelegate.h"
+#import <DeepLinkSDK/BTNDeepLinkManager.h>
+
+@interface BTNReceiverAppDelegate ()
+
+@property (nonatomic, strong) BTNDeepLinkManager *deepLinkManager;
+
+@end
+
 
 @implementation BTNReceiverAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.deepLinkManager = [[BTNDeepLinkManager alloc] init];
+    self.deepLinkManager[@"table/book/1234"] = ^{
+        NSLog(@"deep link");
+    };
+    
+    [self.deepLinkManager handleDeepLink:launchOptions[UIApplicationLaunchOptionsURLKey]
+                       completionHandler:NULL];
+    
+    return YES;
+}
+
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+ 
+    [self.deepLinkManager handleDeepLink:url
+                       completionHandler:NULL];
+    
     return YES;
 }
 
