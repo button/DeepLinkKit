4b7de16803eb57858e1ece81a25f9616b55a3e73
Wes Smith <wes@w5mith.com>
2015-01-09 16:06:00 -0500

    fix sender demo

--- SampleApps/SenderDemo/ActionDataSource/DPLActionDataSource.m
+++ SampleApps/SenderDemo/ActionDataSource/DPLActionDataSource.m
@@ -2,6 +2,7 @@
 #import "NSString+DPLQuery.h"
 #import "NSString+DPLJSON.h"
 #import "DPLDeepLink.h"
+#import "DPLDeepLink+AppLinks.h"
 #import "DPLDemoAction.h"
 
 @interface DPLActionDataSource ()
@@ -48,21 +49,22 @@
 - (DPLDemoAction *)loadOktoberfestAction {
     DPLDemoAction *action = [[DPLDemoAction alloc] init];
     action.actionURL  = [NSURL URLWithString:@"dpl://dpl.com/product/93598"];
-    action.actionName = @"Buy Shiner Oktoberfest";
+    action.actionName = @"Buy: Shiner Oktoberfest";
     return action;
 }
 
 
 - (DPLDemoAction *)logHelloWorldAction {
-    NSDictionary *payload = @{ DPLAppLinkTargetURLKey: @"http://dpl.button.com/say/Congratulations/Button%20Team" };
+    NSDictionary *payload = @{ DPLAppLinksTargetURLKey: @"http://dpl.button.com/say/Hello/World" };
     
-    NSString *payloadString = [[NSString DPL_stringWithJSONObject:payload] DPL_stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *payloadString = [[NSString DPL_stringWithJSONObject:payload]
+                               DPL_stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     NSString *URLString     = [NSString stringWithFormat:@"dpl://deeplink?al_applink_data=%@", payloadString];
     NSURL    *deepLinkURL   = [NSURL URLWithString:URLString];
     
     DPLDemoAction *action = [[DPLDemoAction alloc] init];
     action.actionURL = deepLinkURL;
-    action.actionName = @"Today's Demo";
+    action.actionName = @"Say: “Hello World”";
     return action;
 }
 
