5c155823d140eaa55f2afcd313fc68cc44c6b427
Chris Maddern <chris@chrismaddern.com>
2015-02-06 16:19:15 -0500

    Cleanup

--- DeepLinkSDK/RouteMatcher/DPLRouteMatcher.m
+++ DeepLinkSDK/RouteMatcher/DPLRouteMatcher.m
@@ -2,11 +2,12 @@
 #import "DPLDeepLink_Private.h"
 #import "NSString+DPLTrim.h"
 
-static NSString * const DPLParameterRegexString = @":[a-zA-Z0-9-_]+";
+static NSString * const DPLParameterRoutePattern = @":[a-zA-Z0-9-_]+";
+static NSString * const DPLParameterURLPattern = @"([a-zA-Z0-9-_]+)";
 
 @interface DPLRouteMatcher ()
 
-@property (nonatomic, copy) NSString  *route;
+@property (nonatomic, copy)   NSString  *route;
 @property (nonatomic, strong) NSRegularExpression *regex;
 @property (nonatomic, strong) NSMutableArray *routeParamaterNames;
 
@@ -36,48 +37,53 @@ static NSString * const DPLParameterRegexString = @":[a-zA-Z0-9-_]+";
 - (NSRegularExpression *)regex {
     if (!_regex) {
         _routeParamaterNames = [NSMutableArray array];
-        NSRegularExpression *parameterRegex = [NSRegularExpression regularExpressionWithPattern:DPLParameterRegexString options:0 error:nil];
+        NSRegularExpression *parameterRegex = [NSRegularExpression regularExpressionWithPattern:DPLParameterRoutePattern
+                                                                                        options:0
+                                                                                          error:nil];
         
         __block NSString *modifiedRoute = [self.route copy];
-        [parameterRegex enumerateMatchesInString:self.route
-                                         options:0
-                                           range:NSMakeRange(0, self.route.length)
-                                      usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
-                                          NSString *stringToReplace = [self.route substringWithRange:result.range];
-                                          NSString *variableName = [stringToReplace stringByReplacingOccurrencesOfString:@":" withString:@""];
-                                          [self.routeParamaterNames addObject:variableName];
-                                          
-                                          NSString *replacementRegex = [NSString stringWithFormat:@"([a-zA-Z0-9-_]+)"];
-                                          
-                                          modifiedRoute = [modifiedRoute stringByReplacingOccurrencesOfString:stringToReplace withString:replacementRegex];
-                                      }];
+        NSArray *matches = [parameterRegex matchesInString:self.route
+                                                   options:0
+                                                     range:NSMakeRange(0, self.route.length)];
+        
+        for (NSTextCheckingResult *result in matches) {
+            
+            NSString *stringToReplace   = [self.route substringWithRange:result.range];
+            NSString *variableName      = [stringToReplace stringByReplacingOccurrencesOfString:@":"
+                                                                                     withString:@""];
+            [self.routeParamaterNames addObject:variableName];
+            
+            modifiedRoute = [modifiedRoute stringByReplacingOccurrencesOfString:stringToReplace
+                                                                     withString:DPLParameterURLPattern];
+        }
         
         modifiedRoute = [modifiedRoute stringByAppendingString:@"$"];
-        
-        _regex = [NSRegularExpression regularExpressionWithPattern:modifiedRoute options:0 error:nil];
+        _regex = [NSRegularExpression regularExpressionWithPattern:modifiedRoute
+                                                           options:0
+                                                             error:nil];
     }
+    
     return _regex;
 }
 
 
 - (DPLDeepLink *)deepLinkWithURL:(NSURL *)url {
-    if (!url) {
+    
+    DPLDeepLink *deepLink       = [[DPLDeepLink alloc] initWithURL:url];
+    NSString *deepLinkString    = [deepLink.URL absoluteString];
+    NSArray *matches            = [self.regex matchesInString:deepLinkString
+                                                      options:0
+                                                        range:NSMakeRange(0, deepLinkString.length)];
+    
+    if (!matches.count) {
         return nil;
     }
     
-    DPLDeepLink *deepLink = [[DPLDeepLink alloc] initWithURL:url];
-    
-    NSArray *matches = [self.regex matchesInString:[deepLink.URL absoluteString]
-                                           options:0
-                                             range:NSMakeRange(0, [deepLink.URL absoluteString].length)];
-    
-    if (![matches count]) {
-        return nil;
-    }
+    // Define route parameters in the routeParameters dictionary
     NSMutableDictionary *routeParameters = [NSMutableDictionary dictionary];
     for (NSTextCheckingResult *result in matches) {
         for (int i = 1; i < result.numberOfRanges && i <= self.routeParamaterNames.count; i++) {
-            routeParameters[self.routeParamaterNames[i - 1]] = [[deepLink.URL absoluteString] substringWithRange:[result rangeAtIndex:i]];
+            routeParameters[self.routeParamaterNames[i - 1]] = [deepLinkString substringWithRange:[result rangeAtIndex:i]];
         }
     }
     deepLink.routeParameters = routeParameters;
