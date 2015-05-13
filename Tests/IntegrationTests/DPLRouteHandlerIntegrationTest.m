dc588ea4a830772a566d21d2fa4b59aca085554f
Wes Smith <wes@w5mith.com>
2015-05-13 11:33:53 -0400

    uncomment integration test

--- Tests/IntegrationTests/DPLRouteHandlerIntegrationTest.m
+++ Tests/IntegrationTests/DPLRouteHandlerIntegrationTest.m
@@ -7,10 +7,10 @@
 
 @implementation DPLRouteHandlerIntegrationTest
 
-//- (void)testClassBasedRouteHandlerFlow {
-//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"dpl://dpl.com/product/93598?"]];
-//    [tester waitForViewWithAccessibilityLabel:@"Shiner Oktoberfest"];
-//    [tester waitForViewWithAccessibilityLabel:@"8.99"];
-//}
+- (void)testClassBasedRouteHandlerFlow {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"dpl://dpl.com/product/93598?"]];
+    [tester waitForViewWithAccessibilityLabel:@"Shiner Oktoberfest"];
+    [tester waitForViewWithAccessibilityLabel:@"8.99"];
+}
 
 @end
