f1ab57bd01758189e9989eef095359f952930957
Bay Phillips <bay.phillips@plated.com>
2015-07-16 11:07:26 -0400

    remove unnecessary comments

--- SampleApps/ReceiverDemoSwift/RouteHandlers/DPLMessageRouteHandler.swift
+++ SampleApps/ReceiverDemoSwift/RouteHandlers/DPLMessageRouteHandler.swift
@@ -1,11 +1,3 @@
-//
-//  DPLMessageRouteHandler.swift
-//  DeepLinkKit
-//
-//  Created by Bayrd Philips on 7/16/15.
-//  Copyright (c) 2015 Button, Inc. All rights reserved.
-//
-
 import Foundation
 
 public class DPLMessageRouteHandler: DPLRouteHandler
