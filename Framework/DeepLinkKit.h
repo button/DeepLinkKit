7bbd23ef8a9608505da297551123daad6732934f
Justus Kandzi <justus.kandzi@gmail.com>
2017-02-09 14:51:20 +0100

    add carthage support

---
+++ Framework/DeepLinkKit.h
@@ -0,0 +1,23 @@
+//
+//  DeepLinkKit.h
+//  DeepLinkKit
+//
+//  Created by Justus Kandzi on 09/02/2017.
+//  Copyright © 2017 Button, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeepLinkKit.
+FOUNDATION_EXPORT double DeepLinkKitVersionNumber;
+
+//! Project version string for DeepLinkKit.
+FOUNDATION_EXPORT const unsigned char DeepLinkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeepLinkKit/PublicHeader.h>
+#import <DeepLinkKit/DPLTargetViewControllerProtocol.h>
+#import <DeepLinkKit/DPLDeepLinkRouter.h>
+#import <DeepLinkKit/DPLRouteHandler.h>
+#import <DeepLinkKit/DPLDeepLink.h>
+#import <DeepLinkKit/DPLMutableDeepLink.h>
+#import <DeepLinkKit/DPLErrors.h>
