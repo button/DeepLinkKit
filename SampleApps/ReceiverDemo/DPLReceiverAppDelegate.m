d69fc60dfce96addb988b428df9ba339c9752b16
Alin Radut <alin.claudiu.radut@gmail.com>
2021-01-02 19:12:31 +0200

    Fix for continueUserActivity for Xcode 8.3.3

--- SampleApps/ReceiverDemo/DPLReceiverAppDelegate.m
+++ SampleApps/ReceiverDemo/DPLReceiverAppDelegate.m
@@ -50,7 +50,7 @@
 
 - (BOOL)application:(UIApplication *)application
     continueUserActivity:(NSUserActivity *)userActivity
-    restorationHandler:(void (^ _Nonnull __strong)(NSArray<id<UIUserActivityRestoring>> * _Nullable __strong))restorationHandler {
+    restorationHandler:(void(^)(NSArray * __nullable restorableObjects))restorationHandler {
     
     return [self.router handleUserActivity:userActivity withCompletion:NULL];
 }
